1ce520a63c195a52fe22f313d0b6e1f7385ae81a
Mykhailo Makukha <m.makukha@gmail.com>
2019-07-27 22:11:14 +0300

    Helper functions added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='fabricutil',
+    version='0.1',
+    description='Python Fabric 2 utilities',
+    install_requires=[
+        'fabric>=2.4.0',
+        ],
+    )
